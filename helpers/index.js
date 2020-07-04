c59a7a82709951b888d9973c04e909d8546e461e
dmitriygololobov <dmitriy.gololobov@yandex.com>
2020-07-04 15:20:33 +0700

    Change programming model to classes

--- helpers/index.js
+++ helpers/index.js
@@ -15,11 +15,11 @@ module.exports = {
     }
     return defaultObject
   },
-  generatePayload: (key, expireTime = (1000 * 60 * 60)) => {
+  generatePayload: (key, expireTime = 3600) => {
     return {
       iss: key,
       exp: ((new Date()).getTime() + expireTime)
     }
   },
-  token: (key, secret, expireTime = (1000 * 60 * 60)) => jwt.sign(this.generatePayload(key, expireTime), secret)
+  token: (key, secret, expireTime = 3600) => jwt.sign(this.generatePayload(key, expireTime), secret)
 }
--- index.js
+++ index.js
@@ -2,19 +2,21 @@ const helpers = require('./helpers')
 const api = require('./api')
 const axios = require('axios')
 
-const APIClient = (APIKey = '', APISecret = '', expireTime = (1000 * 60 * 60)) => {
-  this.token = helpers.token(APIKey, APISecret, expireTime)
-  this.basicUrl = 'https://api.zoom.us/v2'
-}
+class APIClient {
 
-APIClient.prototype = {
-  listMeetings: (userEmail = '', params = {}) => {
+  constructor(APIKey = '', APISecret = '', expireTime = 3600) {
+    this.token = helpers.token(APIKey, APISecret, expireTime)
+    this.basicUrl = 'https://api.zoom.us/v2'
+  }
+
+  listMeetings (userEmail = '', params = {}) {
     const clientParams = api.meetings(userEmail, params, this.token, this.basicUrl).listMeetings()
     return axios(clientParams)
   }
+
 }
 
-const init = (APIKey = '', APISecret = '', expireTime = (1000 * 60 * 60)) => new APIClient(APIKey, APISecret, expireTime)
+const init = (APIKey = '', APISecret = '', expireTime = (1000 * 12 * 60 * 60)) => new APIClient(APIKey, APISecret, expireTime)
 
 module.exports = init
 module.exports.APIClient = APIClient
